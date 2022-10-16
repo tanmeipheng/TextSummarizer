a6f20fbc745e119e70c6b021d3f1a9ca27eafdae
tanmeipheng <tanmeipheng@gmail.com>
2022-10-16 19:52:36 +0800

    added train config

--- conf/config.py
+++ conf/config.py
@@ -1,5 +1,20 @@
 class Config(object):
     # ========================================================
+    # Train Settings
+    # ========================================================
+    TRAIN = dict(
+        model_checkpoint="t5-small",
+        dataset_name="cnn_dailymail",
+        dataset_config_name="3.0.0",
+        text_column="article",
+        summary_column="highlights",
+        max_source_length=1024,
+        max_target_length=128,
+        seed=1010,
+        batch_size=1
+    )
+    
+    # ========================================================
     # Predict Settings
     # ========================================================
     PREDICT = dict(
